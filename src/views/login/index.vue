0791a0f29ab5f23db5edc6d91e8171737c14b162
Yang-505 <1573420161@qq.com>
2020-05-17 23:34:30 +0800

    05-完成了表单的基本校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,43 +8,55 @@
         <span class="sub-title">用户登录</span>
       </div>
       <!-- form表单部分 -->
-      <el-form class="login-form">
-        <el-form-item>
+    <el-form class="login-form"
+         :model="loginForm"
+
+         :rules="rules">
+        <el-form-item prop="phone">
           <!-- 手机号 -->
-          <el-input prefix-icon="el-icon-user" placeholder="请输入手机号"></el-input>
+          <el-input
+            v-model="loginForm.phone"
+            prefix-icon="el-icon-user"
+            placeholder="请输入手机号"
+          ></el-input>
         </el-form-item>
-        <el-form-item>
-          <!-- 密码框 -->
-          <el-input v-model="input" show-password prefix-icon="el-icon-lock" placeholder="请输入密码"></el-input>
+        <el-form-item prop="password">
+          <!-- 密码 -->
+          <el-input 
+           v-model="loginForm.password"
+           prefix-icon="el-icon-lock"
+           placeholder="请输入密码"
+            ></el-input>
         </el-form-item>
         <el-form-item>
           <!-- 验证码 -->
-          <el-row :gutter="18">
+          <el-row :gutter="20">
             <el-col :span="16">
-              <el-input placeholder="请输入验证码" prefix-icon="el-icon-key"></el-input>
+              <el-input v-model="loginForm.code" prefix-icon="el-icon-lock" placeholder="请输入验证码"></el-input>
             </el-col>
             <el-col :span="8">
               <img
                 class="captcha"
-                src="http://47.106.148.205/heimamm/public/captcha?type=login"
+                src="http://47.106.148.205/heimamm/public/captcha?type=login&random=0.5041014593676592"
                 alt
               />
             </el-col>
           </el-row>
         </el-form-item>
-       <el-form-item>
+        <el-form-item>
           <el-checkbox></el-checkbox>我已阅读并同意
           <el-link type="primary" href="http://www.baidu.com">用户协议</el-link>和
-          <el-link type="primary" href="http://www.baidu.com">隐私条款</el-link>
+          <el-link type="primary" href="http://www.baidu.com">隐试条款</el-link>
         </el-form-item>
         <el-form-item>
-          <el-button style="width:100%"  type="primary">登录</el-button>
+          <el-button style="width:100%" type="primary">登录</el-button>
         </el-form-item>
-        <el-form-item>
+        <el-form-item label>
           <el-button style="width:100%" type="primary">注册</el-button>
         </el-form-item>
       </el-form>
     </div>
+    <!-- 左侧图片 -->
     <div class="right">
       <img src="@/assets/login_bg.png" alt />
     </div>
@@ -53,10 +65,26 @@
 
 <script>
 export default {
+  name: "Login",
   data() {
     return {
-      //密码输入框
-      input: ""
+      loginForm: {
+        //模型
+        phone: "", //手机号
+        password: "", //密码
+        code: "" //密码
+      },
+      rules: {
+        //校验规则
+        phone: [
+          // //是一个数组,代表可以多个校验规则
+          { required: true, message: "必须输入手机号", trigger: "blur" },
+          { min: 11, max: 11, message: "手机号必须是11位", trigger: "blur" },
+        ],
+        password:[
+            { required: true, message: "必须输入密码", trigger: "blur" },
+        ],
+      }
     };
   }
 };
