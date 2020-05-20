8759360bdf7de29924f3cbe764b6bb825d4192fc
Yang-505 <1573420161@qq.com>
2020-05-20 15:57:57 +0800

    12-完成了登录之后个人信息的获取

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
+import {getToken} from '@/utils/token'
 //导入组件
 import Login from "@/views/login";
 import Layout from "@/views/layout";
@@ -23,6 +24,25 @@ const router = new VueRouter({
         }
     ]
 });
+
+//全局导航守卫
+router.beforeEach((to, from, next) => {
+    // to and from are both route objects. must call `next`.
+    if (to.fullPath == '/login') {
+        next();
+    } else {
+        //你要去非登录页面,这个时候,我们先判断你是否登录了,如果登录了,让你走,没有登录则回到登录页面
+        const token = getToken();
+        if (token) {
+            //代表登录
+            next();
+        } else {
+            alert('token没有,没有登录');
+            next('/login');
+        }
+    }
+});
+
 
 //暴露出去
 export default router;
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,25 @@
 import Vue from 'vue'
 import axios from 'axios'
-
+import { getToken } from '@/utils/token.js'
 //设置基础路径
 
 //暴露 设置基础路径
 axios.defaults.baseURL = process.env.VUE_APP_BASEURL
 
+//请求拦截器
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    const token = getToken();
+    if (token) {
+        config.headers.token = token
+    }
+    return config;
+}, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+});
+
 // 允许携带cookie
-axios.defaults.withCredentials=true
+axios.defaults.withCredentials = true
 
 Vue.prototype.$axios = axios
