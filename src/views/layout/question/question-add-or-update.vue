7e6ce4472dfa2c17a6ff9f54459b219b43ae6dd0
Yang-505 <1573420161@qq.com>
2020-05-28 02:19:39 +0800

    30-新增题目渲染城市完成

--- src/views/layout/question/question-add-or-update.vue
+++ src/views/layout/question/question-add-or-update.vue
@@ -22,22 +22,54 @@
         </el-form-item>
         <el-form-item label="阶段" prop="step">
           <el-select v-model="questionForm.step" placeholder="请选择阶段">
-            <el-option v-for="item in stepObj"
+            <el-option v-for="(value,name) in stepObj" :key="name" :label="value" :value="name"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="企业" prop="enterprise">
+          <el-select v-model="questionForm.enterprise" placeholder="请选择企业">
+            <el-option
+              v-for="item in enterpriseList"
               :key="item.id"
               :label="item.name"
-              :value="item.id">
-            </el-option>
+              :value="item.id"
+            ></el-option>
           </el-select>
-          
-        </el-form-item>       
+        </el-form-item>
+        <el-form-item label="城市" prop="city">
+          <el-cascader
+            size="large"
+            :options="options"
+            :props="{ value: 'label' }"
+            v-model="questionForm.city"
+          ></el-cascader>
+        </el-form-item>
+        <el-form-item label="题型" prop="type">
+          <el-radio-group v-model="questionForm.type">
+            <el-radio v-for="(value, name) in typeObj" :key="name" :label="name">{{value}}</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="难度" prop="difficulty">
+          <el-radio-group v-model="questionForm.difficulty">
+            <el-radio v-for="(value,name) in difficultyObj" :key="name" :label="name">{{value}}</el-radio>
+          </el-radio-group>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button>取消</el-button>
+          <el-button type="primary" @click="submit">确定</el-button>
+        </el-form-item>
       </el-form>
     </el-dialog>
   </div>
 </template>
 
 <script>
+import { regionData } from "element-china-area-data";
 export default {
   name: "QuestionEdit",
+  created() {
+    console.log(regionData);
+  },
   props: {
     subjectList: Array,
     enterpriseList: Array,
@@ -50,20 +82,30 @@ export default {
       mode: "", //add 新增 edit 修改
       //对话框显示/隐藏
       dialogvisible: false,
+      options: regionData,
       questionForm: {
         //这个里面的所有值,将来是传递给服务器的
         subject: "", //学科
-        step:"", //阶段
-        enterprise:"", //企业
-        city:[], //城市
-        type:"1", //题型
-        difficulty:"1", //难度
+        step: "", //阶段
+        enterprise: "", //企业
+        city: [], //城市
+        type: "1", //题型
+        difficulty: "1" //难度
       },
       rules: {
-        subject:{ required: true, message: '请选择学科', trigger: 'blur' },
-        step:{ required: true, message: '请选择阶段', trigger: 'blur' },
+        subject: { required: true, message: "请选择学科", trigger: "blur" },
+        step: { required: true, message: "请选择阶段", trigger: "blur" },
+        enterprise: { required: true, message: "请选择企业", trigger: "blur" },
+        city: [{ required: true, message: "请选择城市", trigger: "change" }],
+        type: [{ required: true, message: "请选择题型", trigger: "change" }],
+        difficulty: [
+          { required: true, message: "请选择难度", trigger: "change" }
+        ]
       }
     };
+  },
+  methods: {
+    submit() {}
   }
 };
 </script>
